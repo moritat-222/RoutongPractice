a08d9562ac8b3d82e263d7a7dc785c9bfb5cdb48
メグミ <morime00@meguminoMacBook-Pro.local>
2020-05-27 00:17:42 +0900

    commit LoginApp

---
+++ nodeJS1/app.js
@@ -0,0 +1,30 @@
+'use strict'
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const viewsPath = path.join(__dirname, './views')
+const publicDirectoryPath = path.join(__dirname, './public')
+
+//テンプレートエンジンの設定
+app.set('view engine', 'ejs');
+app.set('views', viewsPath)
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+  res.render('./index.ejs')
+});
+
+app.get('/login', (req,res) => {
+  res.render('./login.ejs')
+});
+
+app.get('/register', (req, res) => {
+  res.render('./register.ejs')
+})
+
+
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000')
+})
